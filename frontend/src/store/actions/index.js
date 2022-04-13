d8d50cff5fae184287625a3dc71f219db0ebd83c
rfinnegan <81394236+rfinnegan@users.noreply.github.com>
2022-04-13 19:22:15 -0400

    fixed backend

--- frontend/src/store/actions/index.js
+++
@@ -1,7 +0,0 @@
-import {initializeRestaurants, getRestaurantById, createNewRestaurant} from './restaurants'
-import {initializeReviews, getReviewsForRestaurant, createNewReview} from './reviews'
-import {searchRestaurants} from './search'
-
-const actions = {initializeRestaurants, getRestaurantById, createNewRestaurant, initializeReviews, getReviewsForRestaurant, createNewReview, searchRestaurants}
-
-export default actions
